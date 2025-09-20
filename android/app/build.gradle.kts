8573e9234976f663e4a516c4b583322c76a75e34
arpan2580 <adas25150@gmail.com>
2025-09-21 01:13:29 +0530

    gradle bug fix on final release code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,18 +7,18 @@ plugins {
 
 android {
     namespace = "com.example.jnk_app"
-    // compileSdk = flutter.compileSdkVersion
-    compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
+    // compileSdk = 36
     // ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -27,8 +27,8 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        // targetSdk = flutter.targetSdkVersion
-        targetSdk = 36
+        targetSdk = flutter.targetSdkVersion
+        // targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -41,11 +41,11 @@ android {
         }
     }
 
-    packaging {
-        resources {
-            excludes += "META-INF/DEPENDENCIES"
-        }
-    }
+    // packaging {
+        // resources {
+            // excludes += "META-INF/DEPENDENCIES"
+        // }
+    // }
 }
 
 flutter {
